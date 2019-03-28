98165269ee6120b5011ecde383b9e6133f06c982
chenxz <563859095@qq.com>
2019-03-28 10:01:22 +0800

    commit

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Alert, Button, NativeModules, StyleSheet, View} from 'react-native';
+import CodePush from "react-native-code-push";
 import HttpUtil from "./src/utils/HttpUtil";
 
 const commonModule = NativeModules.CommonModule;
@@ -34,6 +35,7 @@ export default class App extends React.Component {
         HttpUtil.post('/user/login', {username: '', password: ''})
             .then((res) => {
                 console.log("success---->" + res)
+                Alert.alert("success", res.code)
             })
             .catch((error) => {
                 console.log("error---->" + JSON.stringify(error))
@@ -44,6 +46,7 @@ export default class App extends React.Component {
         HttpUtil.get('/banner/json')
             .then((res) => {
                 console.log("success---->" + res)
+                Alert.alert("success", res.code)
             })
             .catch((error) => {
                 console.log("error---->" + JSON.stringify(error))
@@ -61,6 +64,47 @@ export default class App extends React.Component {
             </View>
         );
     }
+
+    //如果有更新的提示
+    syncImmediate() {
+        CodePush.sync({
+                //安装模式
+                //ON_NEXT_RESUME 下次恢复到前台时
+                //ON_NEXT_RESTART 下一次重启时
+                //IMMEDIATE 马上更新
+                installMode: CodePush.InstallMode.IMMEDIATE,
+                //对话框
+                updateDialog: {
+                    //是否显示更新描述
+                    appendReleaseDescription: true,
+                    //更新描述的前缀。 默认为"Description"
+                    descriptionPrefix: "更新内容：",
+                    //强制更新按钮文字，默认为continue
+                    mandatoryContinueButtonLabel: "立即更新",
+                    //强制更新时的信息. 默认为"An update is available that must be installed."
+                    mandatoryUpdateMessage: "必须更新后才能使用",
+                    //非强制更新时，按钮文字,默认为"ignore"
+                    optionalIgnoreButtonLabel: '稍后',
+                    //非强制更新时，确认按钮文字. 默认为"Install"
+                    optionalInstallButtonLabel: '后台更新',
+                    //非强制更新时，检查到更新的消息文本
+                    optionalUpdateMessage: '有新版本了，是否更新？',
+                    //Alert窗口的标题
+                    title: '更新提示'
+                },
+            },
+        );
+    }
+
+    componentWillMount() {
+        CodePush.disallowRestart(); // 禁止重启
+        this.syncImmediate(); // 开始检查更新
+    }
+
+    componentDidMount() {
+        CodePush.allowRestart(); // 在加载完了，允许重启
+    }
+
 }
 
 const styles = StyleSheet.create({
